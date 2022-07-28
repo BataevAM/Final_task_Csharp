63a595de6c059eea3ddcc6d3f43c8c0f169bd6ca
«Alexndr» <alex.bat@bk.ru>
2022-07-28 14:50:46 +0300

    add solution of the task

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿string[] specifiedArray = new string[7] {"let", "me", "go", "hello", "world", "404", ":-)"};
+string[] finalArray = new string[specifiedArray.Length];
+
+void ArrayСonversion(string[] specifiedArray, string[] finalArray)
+{
+    int count = 0;
+    for (int i = 0; i < specifiedArray.Length; i++)
+    {
+    if(specifiedArray[i].Length <= 3)
+        {
+        finalArray[count] = specifiedArray[i];
+        count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+ArrayСonversion(specifiedArray, finalArray);
+PrintArray(finalArray);
